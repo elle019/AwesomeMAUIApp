d2d6694268b92012100aacab5572acf564f918cd
Elle <cruz.244819@dasmarinas.sti.edu.ph>
2023-09-22 23:02:42 +0800

    Add project files.

---
+++ AwesomeMAUIApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace AwesomeMAUIApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AwesomeMAUIApp/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace AwesomeMAUIApp;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+  
+    private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+
+     void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+    {
+        string write_type = e.OldTextValue;
+        string display = e.NewTextValue;
+        DisplayLbl.Text = display;
+    }
+	
+}
+
